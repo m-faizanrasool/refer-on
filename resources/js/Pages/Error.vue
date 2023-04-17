f183b699ea0e5034cc7c2cf5bdab8861909fef0c
Shahzaib <baigshahzaib98@gmail.com>
2023-04-17 10:28:38 +0500

    vite build fix.

--- resources/js/Pages/Error.vue
+++ resources/js/Pages/Error.vue
@@ -1,8 +1,6 @@
 <script setup>
 import AppLayout from "@/Layouts/AppLayout.vue";
 
-import { Head } from "@inertiajs/inertia-vue3";
-
 defineProps({
     availableTasks: Array,
     quereyParam: String,
